41312876fb0c40b547203b0d09d5de03ca79ba3f
Kyrene Wang <kyrenewong@163.com>
2017-10-08 21:04:30 +0800

    Update Partition List.java

--- Java/LinkedList/Partition List.java
+++ Java/LinkedList/Partition List.java
@@ -1,3 +1,21 @@
+/**
+ * Example
+ * Given 1->4->3->2->5->2->null and x = 3,
+ * return 1->2->2->4->3->5->null.
+ */
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+
 public class Solution {
     /**
      * @param head: The first node of linked list.
