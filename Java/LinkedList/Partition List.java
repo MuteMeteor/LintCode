4c33ae2e4e220c1d8ffa43657f05da095e9bdbbf
kyrenewong <kyrenewong@163.com>
2017-10-08 21:02:42 +0800

    Partition List
    Partition List

---
+++ Java/LinkedList/Partition List.java
@@ -0,0 +1,34 @@
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param x: an integer
+     * @return: a ListNode
+     */
+    public ListNode partition(ListNode head, int x) {
+        // write your code here
+ 
+        if(head == null || head.next == null)
+            return head;
+ 
+        ListNode fakehead1 = new ListNode(0);
+        ListNode fakehead2 = new ListNode(0);
+        ListNode p1 = fakehead1;
+        ListNode p2 = fakehead2;
+        ListNode p = head;
+ 
+        while(p != null){
+            if(p.val < x){
+                p1.next = new ListNode(p.val);
+                p1 = p1.next;
+            }
+            else{
+                p2.next = new ListNode(p.val);
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        p1.next = fakehead2.next;
+ 
+        return fakehead1.next;
+    }
+}
