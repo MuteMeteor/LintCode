6cf261807f496228cd9fe88b607cd50536a210ae
kyrenewong <kyrenewong@163.com>
2017-10-19 22:08:48 +0800

    20171019
    20171019

--- C++/LinkedList/173 Insertion Sort List.cpp
+++ C++/LinkedList/173 Insertion Sort List.cpp
@@ -6,43 +6,39 @@
  */
 
 /**
- * Definition for ListNode.
- * public class ListNode {
+ * Definition of ListNode
+ * class ListNode {
+ * public:
  *     int val;
- *     ListNode next;
+ *     ListNode *next;
  *     ListNode(int val) {
- *         this.val = val;
- *         this.next = null;
+ *         this->val = val;
+ *         this->next = NULL;
  *     }
  * }
  */
 
-public class Solution {
+class Solution {
+public:
     /**
      * @param head: The first node of linked list.
      * @return: The head of linked list.
      */
-    public ListNode insertionSortList(ListNode head) {
+    ListNode *insertionSortList(ListNode *head) {
         // write your code here
-        if(head == null || head.next == null)
-            return head;
- 
-        ListNode fakehead = new ListNode(0);
- 
-        ListNode p = fakehead;
- 
-        while(head != null){
-            while(p.next != null && p.next.val < head.val)
-                p = p.next;
- 
-            ListNode temp = head.next;
-            head.next = p.next;
-            p.next = head;
- 
-            head = temp;
-            p = fakehead;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* cur = dummy;
+        while(head){
+            cur = dummy;
+            while(cur->next && cur->next->val <= head->val)
+                cur = cur->next;
+            ListNode *tmp = cur->next;
+            cur->next = head;
+            head = head->next;
+            cur->next->next = tmp;
         }
- 
-        return fakehead.next;
+        cur = dummy->next;
+        delete dummy;
+        return cur;
     }
-}
+};
