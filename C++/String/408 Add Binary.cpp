fab304c432428d749965cc81b4c83ec0d37e4165
kyrenewong <kyrenewong@163.com>
2017-10-27 18:06:10 +0800

    20171027
    20171027

--- C++/String/408 Add Binary.cpp
+++ C++/String/408 Add Binary.cpp
@@ -7,45 +7,32 @@
  * Return 100
  */
 
-public class Solution {
+class Solution {
+public:
     /**
      * @param a a number
      * @param b a number
      * @return the result
      */
-    public String addBinary(String a, String b) {
+    string addBinary(string& a, string& b) {
         // Write your code here
-        if(a == null)
-            return b;
-        if(b == null)
-            return a;
- 
-        StringBuilder res = new StringBuilder();
-        int p1 = a.length() - 1;
-        int p2 = b.length() - 1;
+        string res;
         int carry = 0;
- 
-        while(p1 >= 0 || p2 >= 0){
-            int bit1 = 0;
-            if(p1 >= 0){
-                bit1 = a.charAt(p1) - '0';
-                p1--;
-            }
- 
-            int bit2 = 0;
-            if(p2 >= 0){
-                bit2 = b.charAt(p2) - '0';
-                p2--;
-            }
- 
-            int temp = (bit1 + bit2 + carry) % 2;
-            carry = (bit1 + bit2 + carry) / 2;
-            res.insert(0, temp);
+        int i = a.length() - 1, j = b.length() - 1;
+        while (i >= 0 || j >= 0) {
+            int va = 0, vb = 0;
+            if (i >= 0)
+                va = a[i--] - '0';
+            if (j >= 0)
+                vb = b[j--] - '0';
+            int v = va + vb + carry;
+            carry = v/2;
+            res.append(1, (char)(v%2 + '0'));
         }
- 
-        if(carry == 1)
-            res.insert(0, 1);
- 
-        return res.toString();
+        if (carry)
+            res.append(1, (char)(carry + '0'));
+        if (res.length() != 0)
+            reverse(res.begin(), res.end());
+        return res;
     }
-}
+};
