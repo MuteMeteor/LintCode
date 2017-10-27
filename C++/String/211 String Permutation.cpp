31e1ce82fe685cb1cad37ceddca1ba75a1f50c94
kyrenewong <kyrenewong@163.com>
2017-10-27 18:05:03 +0800

    20171027
    20171027

--- C++/String/211 String Permutation.cpp
+++ C++/String/211 String Permutation.cpp
@@ -5,35 +5,19 @@
  */
 
 
-
-
-public class Solution {
+class Solution {
+public:
     /**
      * @param A a string
      * @param B a string
      * @return a boolean
      */
-    public boolean stringPermutation(String A, String B) {
+    bool stringPermutation(string& A, string& B) {
         // Write your code here
-        if(A == null && B == null)
-            return true;
-        if(A == null || B == null)
+        if (A.length() != B.length())
             return false;
- 
-        int[] count = new int[128];
- 
-        if(A.length() != B.length())
-            return false;
- 
-        for(int i = 0; i < A.length(); i++)
-            count[(int) A.charAt(i)]++;
-        for(int i = 0; i < B.length(); i++)
-            count[(int) B.charAt(i)]--;
- 
-        for(int i = 0; i < 128; i++)
-            if(count[i] != 0)
-                return false;
- 
-        return true;
+        sort(A.begin(), A.end());
+        sort(B.begin(), B.end());
+        return A == B;
     }
-}
+};
