b64e0503cc16df187b409dc1063b46300a99dd34
kyrenewong <kyrenewong@163.com>
2017-10-28 09:16:17 +0800

    20171028
    20171028

--- C++/HashTable/547 Intersection of Two Arrays.cpp
+++ C++/HashTable/547 Intersection of Two Arrays.cpp
@@ -9,28 +9,6 @@
 	Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
  */
 
-
-class Solution {
-public:
-    /**
-     * @param nums1 an integer array
-     * @param nums2 an integer array
-     * @return an integer array
-     */
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        // Write your code here
-        vector<int> res;
-        unordered_map<int, bool> mp;
-        for (auto &e : nums1)
-            mp[e] = true;
-        for (auto &e : nums2) {
-            if (mp[e])
-                res.push_back(e);
-            mp[e] = false;
-        }
-        return res;
-    }
-};
  
 class Solution {
 public:
