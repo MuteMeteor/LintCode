dd2c627e9b19059b3d331a593c47ac3d5fece8b9
kyrenewong <kyrenewong@163.com>
2017-10-31 12:21:49 +0800

    20171031
    20171031

---
+++ C++/BreadthFirstSearch/120 Word Ladder.cpp
@@ -0,0 +1,15 @@
+/*
+	Given two words (start and end), and a dictionary, 
+	find the length of shortest transformation sequence from start to end, such that:
+	1. Only one letter can be changed at a time
+	2. Each intermediate word must exist in the dictionary
+
+	Example
+	Given:
+	start = "hit"
+	end = "cog"
+	dict = ["hot","dot","dog","lot","log"]
+	As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+	return its length 5.
+*/
+
